4b9d2c766c0a5d08ea8b81644715152006d06183
Eya Amor <eya.amor@esprit.tn>
2023-02-07 22:44:31 +0100

    first commit

---
+++ AirportManagement/AM.ApplicationCore/Domain/Passenger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Passenger
+    {
+        public DateTime BirthDate { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PassportNumber { get; set; }
+        public string TelNumber { get; set; }
+        public virtual IList<Flight> Flights { get; set; }
+        public virtual string PassengerType { get; }
+
+        public override string ToString()
+        {
+            return " passenger first name : " + FirstName + " last name : " + LastName;
+        }
+    }
+
+}
---
+++ AirportManagement/AM.ApplicationCore/Services/FlightService.cs
@@ -0,0 +1,70 @@
+﻿using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Services
+{
+    public class FlightService: IFlightService
+    {
+
+        ICollection s;
+
+     public FlightService (ICollection source) {
+            s = source;
+                }
+
+        public void ShowFlights(string filterType, string filterValue)
+        {
+            switch (filterType)
+            {
+                case "Destination":
+                    foreach(Flight flight in s)
+                    {
+                        if (flight.Destination == filterValue) {
+                            Console.WriteLine(flight);
+                        }
+                    }
+                    break;
+
+                case "FlightDate":
+                    DateTime d = DateTime.Parse(filterValue);
+                    foreach (Flight flight in s)
+                    {
+                   
+                        if (flight.FlightDate ==d)
+                        {
+                            Console.WriteLine(flight);
+                        }
+                    }
+                    break;
+
+                case "FlightId":
+
+                    int id = Int32.Parse(filterValue);
+
+                    foreach (Flight flight in s)
+                    {
+
+                        if (flight.FlightId == id)
+                        {
+                            Console.WriteLine(flight);
+                        }
+                    }
+                    break;
+                default:
+
+                    throw new ArgumentException("unkown");
+            }
+
+
+           
+           
+        }
+    }
+}
